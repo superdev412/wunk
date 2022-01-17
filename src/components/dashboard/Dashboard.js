35f037020bd9957ee20b8c74aec94c935639ac52
superdev412 <superdeveloper4913@gmail.com>
2022-01-17 05:06:56 +0300

    first commit

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import Header from "../include/header";
+import Introduction from "./introducion";
+import Footer from "../include/footer";
+
+function Dashboard() {
+  return (
+    <div>
+      <Header />
+      <Introduction />
+      {/* <Footer /> */}
+    </div>
+  );
+}
+export default Dashboard
---
+++ src/components/dashboard/introducion.js
@@ -0,0 +1,15 @@
+import '../../assets/css/dashboard/introduction.css';
+import logo from '../../assets/img/logo.png';
+import nft from '../../assets/img/nft1.png'
+
+function Introduction() {
+  return(
+    <div className="content-intro">
+      <img className="logo-img-intro" src={logo} />
+      <br />
+      <img className="nft-img-intro" src={nft} />
+    </div>
+  )
+}
+
+export default Introduction;
